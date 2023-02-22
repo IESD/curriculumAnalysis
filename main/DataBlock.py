c6d74423e84f2a45a8a8f415c83d10f1c590f06e
TomHarrison001 <p2712155@my365.dmu.ac.uk>
2023-02-22 16:09:28 +0000

    Refactored code

--- main/DataBlock.py
+++ main/DataBlock.py
@@ -3,22 +3,22 @@ from collections import defaultdict, OrderedDict
 from pathlib import Path
 
 sw = nltk.corpus.stopwords.words('english')
-punct = string.punctuation + "“”–‘’—¿•"
+punct = string.punctuation + '“”–‘’—¿•'
 
-with open('keywords.txt', encoding="utf8") as f:
+with open('keywords.txt', encoding='utf8') as f:
     keywords = f.read().split(', ')
 
 
 class DataBlock:
-    def __init__(self, raw_data):
-        self.raw_data = raw_data
+    def __init__(self, rawData):
+        self.rawData = rawData
         self.programmes = []
         self.modules = []
-        self.__loadProgrammes(raw_data['0325_Programme_Specification_for_Academics'])
-        self.__loadModules(raw_data['0324_Module_Specification_for_Academics'])
+        self.__loadProgrammes(rawData['0325_Programme_Specification_for_Academics'])
+        self.__loadModules(rawData['0324_Module_Specification_for_Academics'])
 
-    def __loadProgrammes(self, raw_data):
-        linesIter = iter(raw_data)
+    def __loadProgrammes(self, rawData):
+        linesIter = iter(rawData)
         tokens = []
         for token in linesIter:
             tokens.append(token)
@@ -27,8 +27,8 @@ class DataBlock:
                 self.programmes.append(newProgramme)
                 tokens = []
 
-    def __loadModules(self, raw_data):
-        linesIter = iter(raw_data)
+    def __loadModules(self, rawData):
+        linesIter = iter(rawData)
         tokens = []
         for token in linesIter:
             tokens.append(token)
@@ -39,23 +39,23 @@ class DataBlock:
 
 
 class Programme:
-    def __init__(self, raw_data):
-        self.raw_data = nltk.text.Text([token.lower() for token in raw_data])
-        self.data = nltk.text.Text([token.lower() for token in self.raw_data if token not in punct and token not in sw])
-        self.code = raw_data[2]
+    def __init__(self, rawData):
+        self.rawData = nltk.text.Text([token.lower() for token in rawData])
+        self.data = nltk.text.Text([token.lower() for token in self.rawData if token not in punct and token not in sw])
+        self.code = rawData[2]
         self.root = Path('data', f'{self.code}')
         self.keywords = []
-        self.saveRawData(raw_data)
+        self.saveRawData(rawData)
         self.collocations = self.saveCollocations()
         self.concordances = self.saveConcordances()
         self.saveKeywords()
         self.saveKeywordConcordances()
 
-    def saveRawData(self, raw_data):
+    def saveRawData(self, rawData):
         self.root.mkdir(parents=True, exist_ok=True)
         path = self.root / f'{self.code}.txt'
-        with path.open('w', encoding="utf8") as f:
-            print(raw_data, file=f)
+        with path.open('w', encoding='utf8') as f:
+            print(rawData, file=f)
 
     def saveCollocations(self):
         collocations = self.data.collocation_list(num=10, window_size=30)
@@ -70,7 +70,7 @@ class Programme:
         return collocations
 
     def saveConcordances(self):
-        concordances = {self.collocations[i]: self.raw_data.concordance_list(list(self.collocations[i])) for i in
+        concordances = {self.collocations[i]: self.rawData.concordance_list(list(self.collocations[i])) for i in
                         range(5)}
         path = self.root / 'concordances.txt'
         with path.open('w') as f:
@@ -83,22 +83,22 @@ class Programme:
         return concordances
 
     def saveKeywords(self):
-        _vocabs = self.data.vocab()
-        _special_words = defaultdict(dict)
-        for word in _vocabs:
-            relative_frequency = _vocabs[word] / len(self.data) * 10000
-            _special_words[word] = {
-                f'freq': relative_frequency
+        vocabs = self.data.vocab()
+        specialWords = defaultdict(dict)
+        for word in vocabs:
+            relativeFrequency = vocabs[word] / len(self.data) * 10000
+            specialWords[word] = {
+                f'freq': relativeFrequency
             }
         self.root.mkdir(parents=True, exist_ok=True)
         path = self.root / 'keywords.txt'
         with path.open('w') as f:
-            words = OrderedDict(sorted(_special_words.items(), reverse=True))
+            words = OrderedDict(sorted(specialWords.items(), reverse=True))
             keys = words['end'].keys()
             print('-' * 30, file=f)
             print(f'{self.code:30}', file=f)
             print('-' * 30, file=f)
-            print(f"{'word':25}", *[f'{k:10}' for k in keys], sep=' ', file=f)
+            print(f'{"word":25}', *[f'{k:10}' for k in keys], sep=' ', file=f)
             print('-' * 30, file=f)
             for kw in keywords:
                 try:
@@ -110,7 +110,7 @@ class Programme:
             print('', file=f)
 
     def saveKeywordConcordances(self):
-        concordances = {kw: self.raw_data.concordance_list(list(kw)) for kw in self.keywords}
+        concordances = {kw: self.rawData.concordance_list(list(kw)) for kw in self.keywords}
         path = self.root / 'keywordConcordances.txt'
         with path.open('w') as f:
             for term in concordances:
@@ -122,5 +122,5 @@ class Programme:
 
 
 class Module(Programme):
-    def __init__(self, raw_data):
-        super().__init__(raw_data)
+    def __init__(self, rawData):
+        super().__init__(rawData)
--- main/LoadData.py
+++ main/LoadData.py
@@ -6,74 +6,74 @@ from collections import defaultdict, OrderedDict
 from pathlib import Path
 
 
-def extract_data(path):
+def extractData(path):
     for file in os.scandir(path):
         if not file.name.endswith('txt'):
             continue
         name, _ = file.name.rsplit('.', 1)
-        with open(file.path, encoding="utf8") as f:
+        with open(file.path, encoding='utf8') as f:
             tokens = [word for line in f.readlines() for sent in nltk.sent_tokenize(line) for word in
                       nltk.word_tokenize(sent)]
         yield name, tokens
 
 
-root_folder = "../import/"
+rootFolder = '../import/'
 sw = nltk.corpus.stopwords.words('english')
-punct = string.punctuation + "“”–‘’—¿•"
+punct = string.punctuation + '“”–‘’—¿•'
 
-raw_data = {name: tokens for name, tokens in extract_data(root_folder)}
-names = list({name for name in raw_data})
-data = {name: nltk.text.Text([token.lower() for token in raw_data[name] if token not in punct and token not in sw]) for
+rawData = {name: tokens for name, tokens in extractData(rootFolder)}
+names = list({name for name in rawData})
+data = {name: nltk.text.Text([token.lower() for token in rawData[name] if token not in punct and token not in sw]) for
         name in names}
 
-dataBlock = DataBlock(raw_data)
+dataBlock = DataBlock(rawData)
 
 # defining unique words for programmes/modules
-_vocabs = {k: data[k].vocab() for k in data}
-_unique_words = defaultdict(list)
-_special_words = defaultdict(dict)
-for this_name, that_name in zip(names, reversed(names)):
-    for word in _vocabs[this_name]:
-        this_relative_frequency = _vocabs[this_name][word] / len(data[this_name]) * 10000
-        that_relative_frequency = _vocabs[that_name][word] / len(data[that_name]) * 10000
+vocabs = {k: data[k].vocab() for k in data}
+uniqueWords = defaultdict(list)
+specialWords = defaultdict(dict)
+for thisName, thatName in zip(names, reversed(names)):
+    for word in vocabs[thisName]:
+        thisRelativeFrequency = vocabs[thisName][word] / len(data[thisName]) * 10000
+        thatRelativeFrequency = vocabs[thatName][word] / len(data[thatName]) * 10000
         try:
-            relative_frequency_ratio = this_relative_frequency / that_relative_frequency
+            relativeFrequencyRatio = thisRelativeFrequency / thatRelativeFrequency
         except ZeroDivisionError:
-            _unique_words[this_name].append((word, this_relative_frequency))
+            uniqueWords[thisName].append((word, thisRelativeFrequency))
             continue
-        _special_words[this_name][word] = {
-            f'this': this_relative_frequency,
-            f'that': that_relative_frequency,
-            f'ratio': relative_frequency_ratio,
-            f'total': this_relative_frequency + that_relative_frequency
+        specialWords[thisName][word] = {
+            f'this': thisRelativeFrequency,
+            f'that': thatRelativeFrequency,
+            f'ratio': relativeFrequencyRatio,
+            f'total': thisRelativeFrequency + thatRelativeFrequency
         }
-    _unique_words[this_name].sort(key=lambda x: x[1], reverse=True)
+    uniqueWords[thisName].sort(key=lambda x: x[1], reverse=True)
 
 
-def _show_unique_words(path):
+def ShowUniqueWords(path):
     with path.open('w') as f:
-        print("-" * 90, file=f)
-        print(f"unique words", file=f)
-        print("-" * 90, file=f)
-        print(*[f"{n:<42}" for n in names], sep="\t\t", file=f)
-        print("-" * 90, file=f)
+        print('-' * 90, file=f)
+        print(f'unique words', file=f)
+        print('-' * 90, file=f)
+        print(*[f'{n:<42}' for n in names], sep='\t\t', file=f)
+        print('-' * 90, file=f)
         for i in range(10):
-            top = [_unique_words[n][i] for n in names]
-            print(*[f"{word:37} {f:.2f}" for word, f in top], sep="\t\t", file=f)
+            top = [uniqueWords[n][i] for n in names]
+            print(*[f'{word:37} {f:.2f}' for word, f in top], sep='\t\t', file=f)
 
 
-def _show_keywords(key, path):
-    with open('keywords.txt', encoding="utf8") as f:
+def ShowKeywords(key, path):
+    with open('keywords.txt', encoding='utf8') as f:
         keywords = f.read().split(', ')
     with path.open('a') as f:
         for name in names:
-            words = OrderedDict(sorted(_special_words[name].items(), key=key, reverse=True))
+            words = OrderedDict(sorted(specialWords[name].items(), key=key, reverse=True))
             keys = words['report'].keys()
             print('', file=f)
             print('-' * 65, file=f)
             print(name, file=f)
             print('-' * 65, file=f)
-            print(f"{'word':25}", *[f'{k:10}' for k in keys], sep=' ', file=f)
+            print(f'{"word":25}', *[f'{k:10}' for k in keys], sep=' ', file=f)
             print('-' * 65, file=f)
             for kw in keywords:
                 try:
@@ -87,6 +87,6 @@ def _show_keywords(key, path):
 path = Path('data')
 path.mkdir(parents=True, exist_ok=True)
 path = path / 'data.txt'
-_show_unique_words(path)
-_show_keywords(lambda x: x[1]['ratio'], path)
+ShowUniqueWords(path)
+ShowKeywords(lambda x: x[1]['ratio'], path)
 # creates file in directory 'data' containing keywords in modules and programmes

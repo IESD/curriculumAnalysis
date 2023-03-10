6e261c19f6c24b8940db2e03617dc3e2ab9162eb
TomHarrison001 <p2712155@my365.dmu.ac.uk>
2023-03-10 14:08:00 +0000

    Corpus.py uses stemmed tokens for concordances

--- main/corpus.py
+++ main/corpus.py
@@ -2,6 +2,7 @@
 
 import nltk
 from nltk.stem import PorterStemmer
+from collections import defaultdict
 
 stemmer = PorterStemmer()
 
@@ -9,16 +10,23 @@ class Corpus:
     def __init__(self, label, text):
         self.label = label
         self.raw_string = text
-        tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
-        stemmed = [stemmer.stem(t) for t in tokens]
-        self.text = nltk.text.Text(tokens)
-        self.stemmed_text = nltk.text.Text(stemmed)
+        self.tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+        stemmed_tokens = [stemmer.stem(t) for t in self.tokens]
+        self.text = nltk.text.Text(self.tokens)
+        self.stemmed_text = nltk.text.Text(stemmed_tokens)
         self.vocab = self.text.vocab()
         self.stemmed_vocab = self.stemmed_text.vocab()
 
     def analyse(self, keywords, **kwargs):
-        matches = [kw for kw in keywords if kw in self.stemmed_vocab]
-        return [Result(kw, self.stemmed_vocab[kw], self.stemmed_text.concordance_list(kw, **kwargs)) for kw in matches]
+        keywords = [stemmer.stem(kw) for kw in keywords]
+        matches = [kw for kw in keywords if self.stemmed_vocab[kw]]
+        concordances = defaultdict(list)
+        for kw in matches:
+            for stem in self.stemmed_text.concordance_list(kw, **kwargs):
+                t = self.tokens[stem.offset - len(stem.left):stem.offset + len(stem.right) + 1]
+                t[stem.offset] = t[stem.offset].upper()
+                concordances[kw].append(' '.join(t))
+        return [Result(kw, self.stemmed_vocab[kw], concordances[kw]) for kw in matches]
 
     def __repr__(self):
         return f"Corpus({self.label})"
@@ -35,12 +43,12 @@ class Result:
 
 
 if __name__ == "__main__":
-    c = Corpus('test', "hello world and goodbye")
+    c = Corpus('test', 'sustainable sustainability')
     print(c.raw_string)
     print(c.text)
     print(c.vocab)
     print(c)
-    result = c.analyse(['hello', 'goodbye'])
+    result = c.analyse(['sustain'])
     print(result)
     for r in result:
         print('-' * 20)
@@ -48,8 +56,4 @@ if __name__ == "__main__":
         print('-' * 20)
         print(r.concordances)
         for c in r.concordances:
-            if c.left_print:
-                print(c.left_print, end=' ')
-            print(c.query.upper(), end=' ')
-            if c.right_print:
-                print(c.right_print)
+            print(c)

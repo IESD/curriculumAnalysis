a80b587b08c9485cd2bd531ccac37f9063624443
Graeme Stuart <ggstuart@gmail.com>
2023-03-21 17:15:50 +0000

    another refactor - analysis is now done per object

--- main/corpus.py
+++ main/corpus.py
@@ -2,59 +2,37 @@
 
 import nltk
 from nltk.stem import WordNetLemmatizer
-from collections import defaultdict
 
 nltk.download('wordnet')
 lemmatizer = WordNetLemmatizer()
+
+def slice_from_concordance(con):
+    return slice(
+        con.offset - len(con.left),
+        con.offset + len(con.right) + 1
+    )
+
+def delemmatize(concordance, tokens):
+    t = tokens[slice_from_concordance(concordance)]
+    i = len(concordance.left)
+    t[i] = t[i].upper()
+    return " ".join(t)
 
 class Corpus:
     def __init__(self, label, text):
         self.label = label
         self.raw_string = text
         self.tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
-        lemmatized_tokens = [lemmatizer.lemmatize(t) for t in self.tokens]
+        self.lemmatized_tokens = [lemmatizer.lemmatize(t) for t in self.tokens]
         self.text = nltk.text.Text(self.tokens)
-        self.lemmatized_text = nltk.text.Text(lemmatized_tokens)
+        self.lemmatized_text = nltk.text.Text(self.lemmatized_tokens)
         self.vocab = self.text.vocab()
         self.lemmatized_vocab = self.lemmatized_text.vocab()
 
-    def analyse(self, keywords, **kwargs):
-        keywords = [lemmatizer.lemmatize(kw) for kw in keywords]
-        matches = [kw for kw in keywords if self.lemmatized_vocab[kw]]
-        concordances = defaultdict(list)
-        for kw in matches:
-            for stem in self.lemmatized_text.concordance_list(kw, **kwargs):
-                t = self.tokens[stem.offset - len(stem.left):stem.offset + len(stem.right) + 1]
-                t[len(stem.left)] = t[len(stem.left)].upper()
-                concordances[kw].append(' '.join(t))
-        return [Result(kw, self.lemmatized_vocab[kw], concordances[kw]) for kw in matches]
+    def delemmatized_concordance_list(self, kw, **kwargs):
+        lemmatized = self.lemmatized_text.concordance_list(kw, **kwargs)
+        return [delemmatize(l, self.tokens) for l in lemmatized]
 
     def __repr__(self):
         return f"Corpus({self.label})"
 
-
-class Result:
-    def __init__(self, keyword, count, concordances):
-        self.keyword = keyword
-        self.count = count
-        self.concordances = concordances
-
-    def __repr__(self):
-        return f"Result({self.keyword}, {self.count or None})"
-
-
-if __name__ == "__main__":
-    c = Corpus('test', 'sustainable sustainability')
-    print(c.raw_string)
-    print(c.text)
-    print(c.vocab)
-    print(c)
-    result = c.analyse(['sustainable', 'sustainability'])
-    print(result)
-    for r in result:
-        print('-' * 20)
-        print(f'{r.keyword}: {r.count}')
-        print('-' * 20)
-        print(r.concordances)
-        for c in r.concordances:
-            print(c)
--- main/main.py
+++ main/main.py
@@ -1,11 +1,10 @@
 """This is an example script demonstrating the basic functionality"""
 from pathlib import Path
-from csv import DictWriter
-from collections import defaultdict
 
-from txt_parser import file_factory
 from keywords import load_keywords_file
-from corpus import Corpus
+from txt_parser import file_factory
+from exporter import Exporter
+
 
 root = Path(__file__).parent.parent
 output_path = root / 'output'
@@ -13,84 +12,10 @@ import_path = root / "import"
 k = import_path /  'keywords.txt'
 keywords = load_keywords_file(k)
 
-class Analysis:
-    def __init__(self, path, keywords):
-        self.file = file_factory(path)
-        self.keywords = keywords
+p = import_path / "0325_Programme_Specification_for_Academics.txt"
+m = import_path / "0324_Module_Specification_for_Academics.txt"
 
-    @property
-    def type(self):
-        return self.file.type
-
-    def __iter__(self):
-        for obj in self.file:
-            for section, text in obj.corpora().items():
-                corpus = Corpus(section, text)
-                for result in corpus.analyse(self.keywords):
-                    yield obj, section, result
-
-
-class Exporter:
-    def __init__(self, output_path):
-        self.output_path = output_path
-
-    def export(self, analysis):
-        path = self.output_path / f"{analysis.type}s"
-        path.mkdir(exist_ok=True)
-        code_header = f"{analysis.type} code"
-        name = f"{analysis.type} full title"
-        summary_path = path / 'summary.csv'
-        with summary_path.open('w') as summary_file:
-            summary_csv = DictWriter(summary_file, fieldnames=[code_header, name, *analysis.keywords])
-            summary_csv.writeheader()
-            for obj, section, result in analysis:
-                detail_path = path / f"{obj.code}.txt"
-                summary = defaultdict(int)
-                summary[code_header] = obj.code
-                summary[name] = obj.full_title
-                with detail_path.open('w') as detail_file:
-                    summary[result.keyword] += result.count
-                    if result.count:
-                        msg = f'found keyword "{result.keyword}" {result.count} times in "{section}"'
-                        print(f"{'='*len(msg)}\n{msg}\n{'='*len(msg)}\n", file=detail_file)
-                        print("\n\n".join([f"'{c}'" for c in result.concordances]), "\n", file=detail_file)
-                summary_csv.writerow(summary)
-
-
-
-
-def export(file_path):
-    data_file = file_factory(file_path)
-    path = output_path / f"{data_file.type}s"
-    code = f"{data_file.type} code"
-    name = f"{data_file.type} full title"
-    path.mkdir(exist_ok=True)
-    summary_path = path / 'summary.csv'
-    with summary_path.open('w') as summary_file:
-        summary_csv = DictWriter(summary_file, fieldnames=[code, name, *keywords])
-        summary_csv.writeheader()
-        for obj in data_file:
-            detail_path = path / f"{obj.code}.txt"
-            summary = defaultdict(int)
-            summary[code] = obj.code
-            summary[name] = obj.full_title
-            with detail_path.open('w') as detail_file:
-                for section, text in obj.corpora().items():
-                    corpus = Corpus(section, text)
-                    results = corpus.analyse(keywords)
-                    for r in results:
-                        summary[r.keyword] += r.count
-                        if r.count:
-                            msg = f'found keyword "{r.keyword}" {r.count} times in "{section}"'
-                            print(f"{'='*len(msg)}\n{msg}\n{'='*len(msg)}\n", file=detail_file)
-                            print("\n\n".join([f"'{c}'" for c in r.concordances]), "\n", file=detail_file)
-                summary_csv.writerow(summary)
-
-
-if __name__ == "__main__":
-
-    p = import_path / "0325_Programme_Specification_for_Academics.txt"
-    m = import_path / "0324_Module_Specification_for_Academics.txt"
-
-    export(p)
-    export(m)
+for path in [p, m]:
+    file = file_factory(path)
+    exporter = Exporter(file, output_path)
+    exporter.export(keywords)

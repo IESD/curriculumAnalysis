b537ebb2c04c2272cd780f294216f49a8e156e10
TomHarrison001 <p2712155@my365.dmu.ac.uk>
2023-04-22 09:37:18 +0100

    Fix assertion error

--- main/main.py
+++ main/main.py
@@ -1,6 +1,6 @@
 """This is an example script demonstrating the basic functionality"""
 import sys
-from configparser import ConfigParser
+from configparser import ConfigParser       #SafeConfigParser will be removed in Python 3.12, so ConfigParser is recommended
 
 from pathlib import Path
 
@@ -20,8 +20,8 @@ def main(inpath, outpath, keyword_path):
 if __name__ == "__main__":
     assert len(sys.argv) == 2
     config = ConfigParser()
-    config.read("../config.ini")
-    keyword_path = Path(config.get("curriculummAnalysis", "keywords_path"))
-    outpath = Path(config.get("curriculummAnalysis", "outpath"))
+    config.read('../config.ini')
+    keyword_path = Path(config.get('curriculummAnalysis', 'keywords_path'))
+    outpath = Path(config.get('curriculummAnalysis', 'outpath'))
     inpath = Path(sys.argv[1])
     main(inpath, outpath, keyword_path)
